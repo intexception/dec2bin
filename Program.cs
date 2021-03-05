d64eeda6724305299b253d0938565cf29e27ab78
gitus20 <72818559+gitek20@users.noreply.github.com>
2021-03-05 22:33:14 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace bin2dec
+{
+    class Program
+    {
+        // Coded by intexception
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Number to convert: ");
+
+            int number = Int32.Parse(Console.ReadLine());
+
+            string converted = Convert.ToString(number, 2);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Your binary number is: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(converted);
+            Thread.Sleep(-1);
+        }
+
+
+    }
+}
+
